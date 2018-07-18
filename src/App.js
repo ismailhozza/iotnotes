6f84256943d797005de5d235eeef32e6e8554ccd
Hussein Ismail <hussein.ismail.m@gmail.com>
2018-07-18 10:29:39 +0300

    Lomakkeet alku

--- src/App.js
+++ src/App.js
@@ -2,19 +2,55 @@ import React from 'react'
 
 import Note from './components/Note'
 
-const App = ({notes}) => {
-    const rivit = () => notes.map((note) =>
-        <Note key={note.id} note={note} />
-    )
+class App extends React.Component {
 
-    return (
-        <div>
-            <h1>Muistiinpanot</h1>
-            <ul>
-                {rivit()}
-            </ul>
-        </div>
-    )
+    constructor(props) {
+        super(props);
+        this.state = {
+            notes: props.notes,
+            newNote: "uusi muistiinpano"
+        }
+    }
+
+    addNote = (event) => {
+        event.preventDefault()
+        const noteObject = {
+            content: this.state.newNote,
+            date: new Date().toISOString(),
+            important: Math.random() > 0.5,
+            id: this.state.notes.length + 1
+        }
+
+        const notes = this.state.notes.concat(noteObject)
+
+        this.setState({
+            notes: notes,
+            newNote: ''
+        })
+    }
+
+    handleNoteChange = (event) => {
+        console.log(event.target.value)
+        this.setState({ newNote: event.target.value })
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Muistiinpanot</h1>
+                <ul>
+                    {this.state.notes.map((note) => <Note key={note.id} note={note} />)}
+                </ul>
+                <form onSubmit={this.addNote}>
+                    <input
+                        value={this.state.newNote}
+                        onChange={this.handleNoteChange}
+                    />
+                    <button type="submit">tallenna</button>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default App;
